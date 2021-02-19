179f69eff1d04039ff55a27bbd020e23564b11d2
ianclim <ianlim@stanford.edu>
2021-02-18 17:19:53 -0800

    txt file

--- collab.py
+++ collab.py
@@ -12,22 +12,29 @@ __license__ = "MIT"
 def main():
     """ Main entry point of the app """
     """ repo_path structure: {organization}/{repo name}"""
-    repo_path = "facebook/react"
+    #array1 = []
 
-    total = 0
-    counter = 0
-    while True:
-        res = requests.get(
-            "https://api.github.com/repos/" + repo_path + "/contributors",
-            params={'per_page':100, 'anon':'false', 'page':counter}
-        )
-        if len(res.json()) == 0:
-            break
-        else:
-            total += len(res.json())
-            counter += 1
+    with open('projects.txt', 'r') as f:
+        for line in f.readlines():
+            #array1.append(line)
 
-    print("Total number of contributors for repo path: ", repo_path, "is", total)
+            #repo_path = "facebook/react"
+            repo_path = line
+
+            total = 0
+            counter = 0
+            while True:
+                res = requests.get(
+                    "https://api.github.com/repos/" + repo_path + "/contributors",
+                    params={'per_page': 100, 'anon': 'true', 'page': counter}
+                )
+                if len(res.json()) == 0:
+                    break
+                else:
+                    total += len(res.json())
+                    counter += 1
+
+            print("Total number of contributors for repo path: ", repo_path, "is", total)
 
 if __name__ == "__main__":
     """ This is executed when run from the command line """
